4fa902792b83223ceec463f153321d5c27955c67
Adam Chalmers <adam.s.chalmers@gmail.com>
2024-08-13 17:47:46 -0500

    Start reading HTTP requests

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,37 @@
-use std::io::Write;
-use std::net::TcpListener;
+use std::io::{BufRead, BufReader, Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::str::Utf8Error;
+use std::string::FromUtf8Error;
 
-fn handle_client<T: Write>(mut stream: T) {
+fn handle_client(mut stream: TcpStream) {
+    let mut writer = stream.try_clone().unwrap();
+    let mut reader = BufReader::new(stream);
+
+    // Parse headers
+    let mut lines = reader.lines();
+    let first_line = lines.next().unwrap().unwrap();
+    println!("First line: {first_line}");
+    let mut headers = Vec::new();
+    loop {
+        let line = match lines.next().unwrap() {
+            Ok(line) => line,
+            Err(e) => {
+                println!("Error while reading HTTP headers: {e}");
+                return;
+            }
+        };
+        if line.is_empty() {
+            break;
+        }
+        headers.push(line);
+    }
+    println!("Headers: {headers:#?}");
+    let content_length = todo!("Parse the headers and find the content length");
+
+    // Done with headers.
+    let name = "World".to_owned();
     let status = "HTTP/1.1 200 All Good Mate";
-    let body = "你好 world";
+    let body = format!("你好 {name}");
     let body_len = body.as_bytes().len();
     let headers = format!("Content-Length: {}\r\n", body_len);
     let msg = format!("{status}\r\n{headers}\r\n{body}");
@@ -11,7 +39,7 @@ fn handle_client<T: Write>(mut stream: T) {
     let total_bytes = msg_binary.len();
     let mut total_written = 0;
     while total_written < total_bytes {
-        let res = stream.write(&msg_binary[total_written..total_bytes]);
+        let res = writer.write(&msg_binary[total_written..total_bytes]);
         match res {
             Ok(n) => {
                 println!("Wrote {n} bytes");
@@ -34,8 +62,9 @@ fn main() -> std::io::Result<()> {
     for stream in listener.incoming() {
         println!("Accepted request");
         // TODO: Ignore errors listening
-        let stream = stream?;
-        std::thread::spawn(move || {
+        let mut stream = stream?;
+
+        std::thread::spawn(|| {
             handle_client(stream);
         });
     }
