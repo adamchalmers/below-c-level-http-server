1ebad67c30defc1244740e314edf67d1e3a1179a
Adam Chalmers <adam.s.chalmers@gmail.com>
2024-08-20 17:54:08 -0500

    Read the HTTP body, get a name, and then print 'hello <that name>'

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,52 @@
+use std::collections::HashMap;
 use std::io::{BufRead, BufReader, Read, Write};
 use std::net::{TcpListener, TcpStream};
-use std::str::Utf8Error;
-use std::string::FromUtf8Error;
 
-fn handle_client(mut stream: TcpStream) {
+fn handle_client(stream: TcpStream) {
     let mut writer = stream.try_clone().unwrap();
     let mut reader = BufReader::new(stream);
 
     // Parse headers
-    let mut lines = reader.lines();
-    let first_line = lines.next().unwrap().unwrap();
+    let mut line_buf = String::new();
+    if let Err(e) = reader.read_line(&mut line_buf) {
+        eprintln!("Error reading from HTTP request: {e}");
+        return;
+    }
+    // We'll probably need this in the future to keep the HTTP verb.
+    let first_line = line_buf.clone();
     println!("First line: {first_line}");
-    let mut headers = Vec::new();
+    let mut headers = HashMap::new();
     loop {
-        let line = match lines.next().unwrap() {
-            Ok(line) => line,
+        line_buf.clear();
+        match reader.read_line(&mut line_buf) {
+            Ok(_) => {}
             Err(e) => {
-                println!("Error while reading HTTP headers: {e}");
+                eprintln!("Error reading from HTTP request: {e}");
                 return;
             }
-        };
-        if line.is_empty() {
+        }
+        if line_buf == "\r\n" {
             break;
         }
-        headers.push(line);
+        let (k, v) = line_buf.split_once(": ").unwrap();
+        headers.insert(k.to_owned(), v.to_owned());
     }
     println!("Headers: {headers:#?}");
-    let content_length = todo!("Parse the headers and find the content length");
+    let content_length = dbg!(headers.get("Content-Length").unwrap())
+        .trim()
+        .parse()
+        .unwrap();
+    println!("Content length is {content_length}");
 
-    // Done with headers.
-    let name = "World".to_owned();
+    // Read the HTTP body.
+    let mut body_buf = Vec::new();
+    reader
+        .take(content_length)
+        .read_to_end(&mut body_buf)
+        .unwrap();
+    let name = String::from_utf8(body_buf).unwrap();
+
+    // Write the response.
     let status = "HTTP/1.1 200 All Good Mate";
     let body = format!("你好 {name}");
     let body_len = body.as_bytes().len();
@@ -62,7 +79,7 @@ fn main() -> std::io::Result<()> {
     for stream in listener.incoming() {
         println!("Accepted request");
         // TODO: Ignore errors listening
-        let mut stream = stream?;
+        let stream = stream?;
 
         std::thread::spawn(|| {
             handle_client(stream);
