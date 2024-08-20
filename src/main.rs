2a0fc96cbb17dccce9d8e44d5eb62ed45e79b058
Adam Chalmers <adam.s.chalmers@gmail.com>
2024-08-20 18:10:59 -0500

    Neaten up the error handling

--- src/main.rs
+++ src/main.rs
@@ -8,32 +8,34 @@ fn handle_client(stream: TcpStream) -> Result<(), String> {
 
     // Parse headers
     let mut line_buf = String::new();
-    if let Err(e) = reader.read_line(&mut line_buf) {
-        return Err(format!("Could not read first line of HTTP request: {e}"));
-    }
+    reader
+        .read_line(&mut line_buf)
+        .map_err(|e| format!("Could not read first line of HTTP request: {e}"))?;
     // We'll probably need this in the future to keep the HTTP verb.
     let first_line = line_buf.clone();
     println!("First line: {first_line}");
     let mut headers: HashMap<String, String> = HashMap::new();
     loop {
         line_buf.clear();
-        if let Err(e) = reader.read_line(&mut line_buf) {
-            return Err(format!("Error reading from HTTP request: {e}"));
-        }
+        reader
+            .read_line(&mut line_buf)
+            .map_err(|e| format!("Error reading from HTTP request: {e}"))?;
         if line_buf == "\r\n" {
             break;
         }
         let (k, v) = match line_buf.split_once(": ") {
             Some((k, v)) => (k.to_owned(), v.to_owned()),
-            None => return Err(format!("Invalid HTTP header (missing a ': ')")),
+            None => return Err(format!("Invalid HTTP header {line_buf} (missing a ': ')")),
         };
         headers.insert(k, v);
     }
     println!("Headers: {headers:#?}");
-    let content_length = dbg!(headers.get("Content-Length").unwrap())
+    let content_length = headers
+        .get("Content-Length")
+        .ok_or("Missing content-length header".to_owned())?
         .trim()
         .parse()
-        .unwrap();
+        .map_err(|e| format!("Content-length header was not a valid number: {e}"))?;
     println!("Content length is {content_length}");
 
     // Read the HTTP body.
@@ -41,8 +43,9 @@ fn handle_client(stream: TcpStream) -> Result<(), String> {
     reader
         .take(content_length)
         .read_to_end(&mut body_buf)
-        .unwrap();
-    let name = String::from_utf8(body_buf).unwrap();
+        .map_err(|e| format!("IO error, could not read the HTTP body: {e}"))?;
+    let name = String::from_utf8(body_buf)
+        .map_err(|e| format!("HTTP body was not a UTF-8 string: {e}"))?;
 
     // Write the response.
     let status = "HTTP/1.1 200 All Good Mate";
