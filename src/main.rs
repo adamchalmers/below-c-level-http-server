81c609004164f280f3e10dd43b07ada58dbb2a8c
Adam Chalmers <adam.s.chalmers@gmail.com>
2024-08-20 18:02:11 -0500

    Improve error handling

--- src/main.rs
+++ src/main.rs
@@ -2,34 +2,32 @@ use std::collections::HashMap;
 use std::io::{BufRead, BufReader, Read, Write};
 use std::net::{TcpListener, TcpStream};
 
-fn handle_client(stream: TcpStream) {
+fn handle_client(stream: TcpStream) -> Result<(), String> {
     let mut writer = stream.try_clone().unwrap();
     let mut reader = BufReader::new(stream);
 
     // Parse headers
     let mut line_buf = String::new();
     if let Err(e) = reader.read_line(&mut line_buf) {
-        eprintln!("Error reading from HTTP request: {e}");
-        return;
+        return Err(format!("Could not read first line of HTTP request: {e}"));
     }
     // We'll probably need this in the future to keep the HTTP verb.
     let first_line = line_buf.clone();
     println!("First line: {first_line}");
-    let mut headers = HashMap::new();
+    let mut headers: HashMap<String, String> = HashMap::new();
     loop {
         line_buf.clear();
-        match reader.read_line(&mut line_buf) {
-            Ok(_) => {}
-            Err(e) => {
-                eprintln!("Error reading from HTTP request: {e}");
-                return;
-            }
+        if let Err(e) = reader.read_line(&mut line_buf) {
+            return Err(format!("Error reading from HTTP request: {e}"));
         }
         if line_buf == "\r\n" {
             break;
         }
-        let (k, v) = line_buf.split_once(": ").unwrap();
-        headers.insert(k.to_owned(), v.to_owned());
+        let (k, v) = match line_buf.split_once(": ") {
+            Some((k, v)) => (k.to_owned(), v.to_owned()),
+            None => return Err(format!("Invalid HTTP header (missing a ': ')")),
+        };
+        headers.insert(k, v);
     }
     println!("Headers: {headers:#?}");
     let content_length = dbg!(headers.get("Content-Length").unwrap())
@@ -63,12 +61,11 @@ fn handle_client(stream: TcpStream) {
                 total_written += n;
             }
             Err(e) => {
-                eprintln!("Could not write request: {e}");
-                return;
+                return Err(format!("Could not write request: {e}"));
             }
         }
     }
-    println!("Request hung up");
+    Ok(())
 }
 
 fn main() -> std::io::Result<()> {
@@ -82,7 +79,9 @@ fn main() -> std::io::Result<()> {
         let stream = stream?;
 
         std::thread::spawn(|| {
-            handle_client(stream);
+            if let Err(e) = handle_client(stream) {
+                eprintln!("{e}");
+            }
         });
     }
     Ok(())
