0859ec7683121cec626db70c04498a7cb8a67185
Adam Chalmers <adam.s.chalmers@gmail.com>
2024-08-28 08:13:53 -0500

    Solo bugfixes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
+use std::cmp::min;
 use std::collections::HashMap;
 use std::io::{BufRead, BufReader, Read, Write};
 use std::net::{TcpListener, TcpStream};
+
+// On MacOS, the `write` syscall can only handle a certain number
+// of bytes at once. Anything larger will return OS Error 22,
+// i.e. EINVAL.
+// From the `man 2 write`:
+//   write() and pwrite() will fail if the parameter nbyte exceeds INT_MAX, and they do not attempt a partial
+//   write.
+// So, let's limit it to MAX_INT. I imported MAX_INT in C and printed it, and it was
+// this.
+/// Maximum number of bytes that can be written in a single `write` call.
+const MAX_WRITE_LEN: usize = i32::MAX as usize;
 
 fn handle_client(stream: TcpStream) -> Result<(), String> {
     let writer = stream.try_clone().unwrap();
@@ -42,14 +54,14 @@ fn respond<W: Write, R: BufRead>(mut writer: W, mut reader: R) -> Result<(), Str
         .map_err(|e| format!("Content-length header was not a valid number: {e}"))?;
     println!("Content length is {content_length}");
 
-    let mut first_line_sent = false;
     if let Some(v) = headers.get("Expect") {
         if v == "100-continue" {
             // See https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/100
-            let first_line_resp = b"HTTP/1.1 100 Continue\r\n";
+            // Note that the server must still write a final response status
+            // after it reads the HTTP body.
+            let first_line_resp = b"HTTP/1.1 100 Continue\r\n\r\n";
             let n = writer.write(first_line_resp).unwrap();
             assert_eq!(n, first_line_resp.len());
-            first_line_sent = true;
         }
     }
 
@@ -66,20 +78,18 @@ fn respond<W: Write, R: BufRead>(mut writer: W, mut reader: R) -> Result<(), Str
     let body = format!("你好 {name}");
     let body_len = body.as_bytes().len();
     let headers = format!("Content-Length: {}\r\n", body_len);
-    let msg = if first_line_sent {
-        format!("{headers}\r\n{body}")
-    } else {
-        format!("{status}\r\n{headers}\r\n{body}")
-    };
+    let msg = format!("{status}\r\n{headers}\r\n{body}");
     let msg_binary = msg.as_bytes();
-    let total_bytes = dbg!(msg_binary.len());
+    let total_bytes = msg_binary.len();
     let mut total_written = 0;
-    const MB: usize = 1_000_000;
     let mut num_writes = 0;
     while total_written < total_bytes {
-        let write_cap = std::cmp::max(MB, body_len);
+        // Don't write more than `limit` bytes at once.
+        let write_cap = min(MAX_WRITE_LEN, body_len);
+        // Don't write more bytes than remain in the body!
+        let write_cap = min(write_cap, total_bytes - total_written);
 
-        let write_fragment = &msg_binary[dbg!(total_written..write_cap)];
+        let write_fragment = &msg_binary[total_written..total_written + write_cap];
         let res = writer.write(write_fragment);
         match res {
             Ok(n) => {
