000fb63cb05a8816aa9e3a9ec060764fc1994fce
Adam Chalmers <adam.s.chalmers@gmail.com>
2024-08-27 19:43:27 -0500

    Handle 'Expect' header
    We're trying to debug an "OS error 22" when sending large files

--- src/main.rs
+++ src/main.rs
@@ -42,35 +42,55 @@ fn respond<W: Write, R: BufRead>(mut writer: W, mut reader: R) -> Result<(), Str
         .map_err(|e| format!("Content-length header was not a valid number: {e}"))?;
     println!("Content length is {content_length}");
 
+    let mut first_line_sent = false;
+    if let Some(v) = headers.get("Expect") {
+        if v == "100-continue" {
+            // See https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/100
+            let first_line_resp = b"HTTP/1.1 100 Continue\r\n";
+            let n = writer.write(first_line_resp).unwrap();
+            assert_eq!(n, first_line_resp.len());
+            first_line_sent = true;
+        }
+    }
+
     // Read the HTTP body.
     let mut body_buf = Vec::new();
     reader
         .take(content_length)
         .read_to_end(&mut body_buf)
         .map_err(|e| format!("IO error, could not read the HTTP body: {e}"))?;
-    let name = String::from_utf8(body_buf)
-        .map_err(|e| format!("HTTP body was not a UTF-8 string: {e}"))?;
 
     // Write the response.
+    let name = String::from_utf8_lossy(&body_buf);
     let status = "HTTP/1.1 200 All Good Mate";
     let body = format!("你好 {name}");
     let body_len = body.as_bytes().len();
     let headers = format!("Content-Length: {}\r\n", body_len);
-    let msg = format!("{status}\r\n{headers}\r\n{body}");
+    let msg = if first_line_sent {
+        format!("{headers}\r\n{body}")
+    } else {
+        format!("{status}\r\n{headers}\r\n{body}")
+    };
     let msg_binary = msg.as_bytes();
-    let total_bytes = msg_binary.len();
+    let total_bytes = dbg!(msg_binary.len());
     let mut total_written = 0;
+    const MB: usize = 1_000_000;
+    let mut num_writes = 0;
     while total_written < total_bytes {
-        let res = writer.write(&msg_binary[total_written..total_bytes]);
+        let write_cap = std::cmp::max(MB, body_len);
+
+        let write_fragment = &msg_binary[dbg!(total_written..write_cap)];
+        let res = writer.write(write_fragment);
         match res {
             Ok(n) => {
-                println!("Wrote {n} bytes");
+                println!("Wrote {n} bytes (write #{num_writes})");
                 total_written += n;
             }
             Err(e) => {
-                return Err(format!("Could not write request: {e}"));
+                return Err(format!("Could not write request: {e:?}"));
             }
         }
+        num_writes += 1;
     }
     Ok(())
 }
